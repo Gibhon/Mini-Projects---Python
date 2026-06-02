e51e4babb9eb4ea60d84494c74d283ba6d26e212
Gibhon <gibhonofficial@gmail.com>
2026-06-02 23:35:49 +0545

    feat(classifier): multiclass wine classifier with train/val pipeline

--- wine_grade_classfier/src/main.py
+++ wine_grade_classfier/src/main.py
@@ -20,7 +20,87 @@ def load_data(file_path : str) -> np.ndarray:
     labels_np_collapsed = np.select(conditions, choices)
     
     features_np_final = features_np.astype(np.float32)
-    labels_np_final = features_np.astype(np.float64)
+    labels_np_final = labels_np_collapsed.astype(np.int64)
     
     return features_np_final, labels_np_final
+
+
+class WineDataset(Dataset):
+    def __init__(self, features, labels):
+        super().__init__()
+        features = torch.tensor(features, dtype=torch.float32) #dtype not necessary here tho
+        self.labels = torch.tensor(labels, dtype = torch.long)
+        self.features = (features - features.mean(dim = 0)) / features.std(dim = 0)
+        
+    def __len__(self):
+        return len(self.features)
+    
+    def __getitem__(self, index):
+        return self.features[index], self.labels[index]
+    
+class WineClassifier(nn.Module):
+    def __init__(self, n_inputs, n_neurons):
+        super().__init__()
+        self.layer1 = nn.Linear(in_features=  n_inputs, out_features= n_neurons)
+        self.layer2 = nn.Linear(in_features= n_neurons, out_features= 3)
+        self.relu = nn.ReLU()        
+    def forward(self, inputs):
+        inputs = self.layer1(inputs)
+        inputs = self.relu(inputs)
+        inputs = self.layer2(inputs)
+        return inputs
+    
+def train_model(n_epoch, model, train_set_loader, loss_fn, optimizer):
+    for epoch in range(n_epoch):
+        total_loss = 0
+        n_accurate = 0
+        n_total = 0
+        for x_batch, y_batch in train_set_loader:
+            optimizer.zero_grad(set_to_none = True)
+            prediction_dim3 = model.forward(x_batch)
+            prediction_final = torch.argmax(prediction_dim3, dim = 1)
+            loss = loss_fn(prediction_dim3, y_batch)
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+            n_accurate += (prediction_final == y_batch).sum().item()
+            n_total += prediction_final.size(0)
+        if (epoch % (n_epoch/10) == 0):
+            print(f"Loss : {total_loss/len(train_set_loader)}")
+            print(f"Accuracy: {n_accurate / n_total}")
+    torch.save(model.state_dict(), r"C:\Users\Acer\Documents\Code-Main\Python\Mini-Projects\wine_grade_classfier\data\weights.pth")
+
+def val_model(model, val_set_loader, loss_fn):
+    model.load_state_dict(torch.load(r"C:\Users\Acer\Documents\Code-Main\Python\Mini-Projects\wine_grade_classfier\data\weights.pth"))
+    total_loss = 0
+    n_accurate = 0
+    n_total = 0
+    with torch.no_grad():
+        for x_batch, y_batch in val_set_loader:
+            prediction_dim3 = model.forward(x_batch)
+            prediction_final = torch.argmax(prediction_dim3, dim = 1)
+            loss = loss_fn(prediction_dim3, y_batch)
+            total_loss += loss.item()
+            n_accurate += (prediction_final == y_batch).sum().item()
+            n_total += prediction_final.size(0)
+            
+    print(f"loss : {total_loss/ len(val_set_loader)}")
+    print(f"Accuracy: {n_accurate / n_total}")
+
+
+features_np, labels_np = load_data(r"C:\Users\Acer\Documents\Code-Main\Python\Mini-Projects\wine_grade_classfier\data\winequality-red.csv")
+data_set = WineDataset(features_np, labels_np)
+train_set, val_set = random_split(data_set, lengths=[.8, .2], generator=torch.Generator().manual_seed(42))
+
+train_set_loader = DataLoader(train_set, shuffle=True, batch_size=32)
+val_set_loader = DataLoader(val_set, shuffle=False, batch_size=32)
+
+torch.manual_seed(42)
+model = WineClassifier(11, 20)
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr = 0.005)
+
+# train_model(100, model, train_set_loader, loss_fn, optimizer)
+
+val_model(model, val_set_loader, loss_fn)
 
