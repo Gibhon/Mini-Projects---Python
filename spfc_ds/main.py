bc1481c8bc3e33c6e1539654490e053f8c791b82
Gibhon <gibhonofficial@gmail.com>
2026-06-28 20:18:38 +0545

    feat:Modification for 7% val acc increase

--- spfc_ds/main.py
+++ spfc_ds/main.py
@@ -1,7 +1,6 @@
 import torch
 from torch import nn
-from torch.utils.data import Dataset, DataLoader
-from torch.utils.data import random_split
+from torch.utils.data import DataLoader, Dataset, random_split
 
 eps = 1e-7
 
@@ -19,7 +18,7 @@ x = torch.tensor(
 mean_x = x.mean(dim=0)
 std_x = x.std(dim=0)
 x_scaled = (x - mean_x) / (std_x + eps)
-y = torch.tensor([1.0, 1.0, 1.0, 0.0, 0.0, 0.0], dtype=torch.float32)  
+y = torch.tensor([1.0, 1.0, 1.0, 0.0, 0.0, 0.0], dtype=torch.float32)
 
 
 class StudentDataset(Dataset):
@@ -27,62 +26,60 @@ class StudentDataset(Dataset):
         super().__init__()
         self.features = features
         self.labels = labels
-        
+
     def __len__(self):
         return len(self.features)
-    
+
     def __getitem__(self, index):
         x = self.features[index]
         y = self.labels[index]
-        
+
         return x, y
+
 
 dataset = StudentDataset(x_scaled, y)
 train_set, val_set = random_split(
-    dataset, 
-    lengths=[0.8, 0.2],
-    generator=torch.Generator().manual_seed(42)
+    dataset, lengths=[0.8, 0.2], generator=torch.Generator().manual_seed(42)
 )
 train_loader = DataLoader(train_set, batch_size=32, shuffle=True)
 val_loader = DataLoader(val_set, batch_size=32, shuffle=True)
 
 
 class StudentClassifier(nn.Module):
-    def __init__(self, n_inputs : int, n_neurons : int):
-        super().__init__() 
-        self.layer1 = nn.Linear(in_features= n_inputs, out_features= n_neurons) 
-        self.layer2 = nn.Linear(n_neurons, 1) 
+    def __init__(self, n_inputs: int, n_neurons: int):
+        super().__init__()
+        self.layer1 = nn.Linear(in_features=n_inputs, out_features=n_neurons)
+        self.layer2 = nn.Linear(n_neurons, 1)
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
-            
-    def forward(self, inputs : torch.Tensor):
+
+    def forward(self, inputs: torch.Tensor):
         inputs = self.layer1(inputs)
         inputs = self.relu(inputs)
         inputs = self.layer2(inputs)
         return self.sigmoid(inputs)
-    
+
+
 torch.manual_seed(42)
 model = StudentClassifier(2, 4)
 
 loss_fn = nn.BCELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr = 1)
+optimizer = torch.optim.SGD(model.parameters(), lr=1)
 
 for epoch in range(100):
     epoch_loss = 0
     for x_batch, y_batch in train_loader:
-        optimizer.zero_grad(set_to_none= True)
-    
+        optimizer.zero_grad(set_to_none=True)
+
         prediction = model(x_batch)
-    
+
         loss = loss_fn(prediction, y_batch.unsqueeze(1))
-    
+
         loss.backward()
         optimizer.step()
-        
+
         epoch_loss += loss.item()
-        
- 
-        
+
     val_loss = 0
 
     with torch.no_grad():
@@ -91,4 +88,6 @@ for epoch in range(100):
             loss = loss_fn(predictions, y_batch.unsqueeze(1))
             val_loss += loss.item()
     if epoch % 10 == 0:
-        print(f"Epoch {epoch} | Train Loss: {epoch_loss:.4f} | Val Loss: {val_loss:.4f}")
+        print(
+            f"Epoch {epoch} | Train Loss: {epoch_loss:.4f} | Val Loss: {val_loss:.4f}"
+        )
--- wine_grade_classfier/src/main.py
+++ wine_grade_classfier/src/main.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import numpy as np
 import torch
 from torch import nn
@@ -38,22 +40,26 @@ class WineDataset(Dataset):
 
 
 class WineClassifier(nn.Module):
-    def __init__(self, n_inputs, n_neurons):
+    def __init__(self, n_inputs, n_neurons1, n_neurons2):
         super().__init__()
-        self.layer1 = nn.Linear(in_features=n_inputs, out_features=n_neurons)
-        self.layer2 = nn.Linear(in_features=n_neurons, out_features=3)
+        self.layer1 = nn.Linear(in_features=n_inputs, out_features=n_neurons1)
+        self.layer2 = nn.Linear(in_features=n_neurons1, out_features=n_neurons2)
+        self.layer3 = nn.Linear(n_neurons2, 3)
         self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(p=0.1)
+        self.dropout1 = nn.Dropout(p=0.1)
+        self.dropout2 = nn.Dropout(p=0.2)
 
     def forward(self, inputs):
         inputs = self.layer1(inputs)
         inputs = self.relu(inputs)
-        inputs = self.dropout(inputs)
+        inputs = self.dropout1(inputs)
         inputs = self.layer2(inputs)
+        inputs = self.layer3(self.dropout2(self.relu(inputs)))
         return inputs
 
 
-def train_model(n_epoch, model, train_set_loader, loss_fn, optimizer):
+def train_model(n_epoch, model, train_set_loader, loss_fn, optimizer, path):
+    min_loss = 100
     for epoch in range(n_epoch):
         total_loss = 0
         n_accurate = 0
@@ -68,24 +74,41 @@ def train_model(n_epoch, model, train_set_loader, loss_fn, optimizer):
             total_loss += loss.item()
             n_accurate += (prediction_final == y_batch).sum().item()
             n_total += prediction_final.size(0)
+        current_loss = total_loss / len(train_set_loader)
+        if min_loss > current_loss:
+            min_loss = current_loss
+            min_epoch = epoch
+            checkpoint = {
+                "epoch": epoch + 1,
+                "model_state_dict": model.state_dict(),
+                "optimizer_state_dict": optimizer.state_dict(),
+            }
+
         if epoch % (n_epoch / 10) == 0:
-            print(f"Loss : {total_loss / len(train_set_loader)}")
+            print(f"Loss : {current_loss}")
             print(f"Accuracy: {n_accurate / n_total}")
-    torch.save(
-        model.state_dict(),
-        r"C:\Users\Acer\Documents\Code-Main\Python\Mini-Projects\wine_grade_classfier\data\weights.pth",
-    )
+    print(f"MinLoss: {min_loss}")
+    print(min_epoch)
+    torch.save(checkpoint, path)
+    return min_loss
+    # torch.save(
+    #     model.state_dict(),
+    #     r"C:\Users\Acer\Documents\Code-Main\Python\Mini-Projects\wine_grade_classfier\data\weights.pth",
+    # )
 
 
-def val_model(model, val_set_loader, loss_fn):
-    model.load_state_dict(
-        torch.load(
-            r"C:\Users\Acer\Documents\Code-Main\Python\Mini-Projects\wine_grade_classfier\data\weights.pth"
-        )
-    )
+def val_model(model, val_set_loader, loss_fn, path):
+    # 1. Load the full checkpoint dictionary
+    checkpoint = torch.load(path, weights_only=True)
+
+    # 2. Extract and load ONLY the model weights
+    model.load_state_dict(checkpoint["model_state_dict"])
+
     total_loss = 0
     n_accurate = 0
     n_total = 0
+
+    model.eval()
     with torch.no_grad():
         for x_batch, y_batch in val_set_loader:
             prediction_dim3 = model.forward(x_batch)
@@ -94,13 +117,13 @@ def val_model(model, val_set_loader, loss_fn):
             total_loss += loss.item()
             n_accurate += (prediction_final == y_batch).sum().item()
             n_total += prediction_final.size(0)
-
+    print("--------------------------------------------")
     print(f"loss : {total_loss / len(val_set_loader)}")
     print(f"Accuracy: {n_accurate / n_total}")
 
 
 features_np, labels_np = load_data(
-    r"C:\Users\Acer\Documents\Code-Main\Python\Mini-Projects\wine_grade_classfier\data\winequality-red.csv"
+    Path(__file__).resolve().parent.parent / "data" / "winequality-red.csv"
 )
 data_set = WineDataset(features_np, labels_np)
 train_set, val_set = random_split(
@@ -111,10 +134,15 @@ train_set_loader = DataLoader(train_set, shuffle=True, batch_size=32)
 val_set_loader = DataLoader(val_set, shuffle=False, batch_size=32)
 
 torch.manual_seed(42)
-model = WineClassifier(11, 30)
+model = WineClassifier(11, 16, 16)
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=1e-4)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-4)
 
-# train_model(100, model, train_set_loader, loss_fn, optimizer)
+model_location = Path(__file__).resolve().parent.parent / "data" / "model.pth"
 
-val_model(model, val_set_loader, loss_fn)
+min_loss = train_model(90, model, train_set_loader, loss_fn, optimizer, model_location)
+
+val_model(model, val_set_loader, loss_fn, model_location)
+
+# loss : 0.8093988776206971
+# Accuracy: 0.6489028213166145
